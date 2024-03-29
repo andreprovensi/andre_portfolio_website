a1a221cf293e00c80ddbfa4dd8b4dfcf3676ddf6
andreprovensi <andreprovensi@gmail.com>
2024-03-29 20:42:43 -0300

    finished home section

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import '../static/css/Home.css'
+import '../public/images/andre_profile.png'
+import profilePicture from '../public/images/andre_profile.png'
 
 function HomeSection({ lang }) {
+  
+  
+  const texts = {
+    homeText:{
+      en:"Hi, I'm André",
+      pt:"André Provensi"
+    },
+    aboutText:{
+      en:"I'm a Data Scientist",
+      pt:'Cientista de Dados'
+    },
+  }
+
   return (
     <section>
-        
+
+      <div className='dislocated-div'>
+        <div className='home-text'>
+          <p id='name'>{texts.homeText[lang]}</p>
+          <p id='description'>{texts.aboutText[lang]}</p>
+        </div>
+
+        <div className='personal-image'>
+          <img src={profilePicture} alt="profile-image" />
+        </div>
+      </div>
+      
     </section>
   )
 }
