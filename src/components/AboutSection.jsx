b05456dec73ef5492f3e7ff521161e2290a48dca
andreprovensi <andreprovensi@gmail.com>
2024-04-14 20:05:45 -0300

    improving responsivity

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import '../static/css/About.css'
 import { AiOutlineUser } from "react-icons/ai";
 import { DiCodeBadge } from "react-icons/di";
+import { BsMortarboard } from "react-icons/bs";
+
 
 function AboutSection({ lang }) {
   
@@ -13,10 +15,21 @@ function AboutSection({ lang }) {
           developing my skills in ETL processes and data integration.My work involved wrangling diverse datasets, ranging in size and complexity, to uncover actionable insights and
           develop robust machine learning models. I also have good interdisciplinary communication skills, demonstrated through my ability
           to explain technical concepts and present insights to diverse audiences with varying backgrounds`,
-          pt:"André Provensi"
+          pt:`Cientista de dados com experiência em machine learning, análise de dados e proficiência
+          em Python, PysPark, Pandas, Numpy, Scikit-learn, SQL, AWS e PowerBI.
+          Apliquei Ciência de Dados, Inteligência Artificial e Aprendizado de Máquina para apoiar a
+          tomada de decisões e resolver problemas de negócios relacionados à detecção de
+          fraudes, segmentação de clientes, comportamento do cliente e relação cliente-produto.
+          Trabalhei em colaboração com equipes de fraude, marketing e produto, o que me permitiu
+          ter contato com diversos tipos e tamanhos de dados, desenvolvendo habilidades sólidas
+          em ETL, integração e manipulação de diferentes bancos de dados para descobrir insights
+          e construir modelos de aprendizado de máquina.
+          A diversidade de equipes com as quais trabalhei me proporcionou habilidades sólidas de
+          comunicação para explicar conceitos técnicos e relatar insights e resultados para pessoas
+          de diferentes formações e áreas de negócio`
         },
 
-        skillsText:{
+        skillsList:{
           en:[
             'Python',
             'SQL',
@@ -25,8 +38,49 @@ function AboutSection({ lang }) {
             'Machine Learning',
             'Statistics'
           ],
-          pt:'Cientista de Dados'
+          pt:[
+            'Python',
+            'SQL',
+            'AWS',
+            'Power BI',
+            'Machine Learning',
+            'Estatística'
+          ],
         },
+
+        educationList:{
+          en:[
+            'Python',
+            'SQL',
+            'AWS',
+            'Power BI',
+            'Machine Learning',
+            'Statistics'
+          ],
+          pt:[
+            'Python',
+            'SQL',
+            'AWS',
+            'Power BI',
+            'Machine Learning',
+            'Estatística'
+          ],
+        },
+
+        aboutText:{
+            'en':'ABOUT',
+            'pt':'SOBRE'
+        },
+        
+        skillsText:{
+            'en':'SKILLS',
+            'pt':'SKILLS'
+        },
+        educationText:{
+            'en':'EDUCATION',
+            'pt':'FORMAÇÃO'
+        },
+
       }
 
     return (
@@ -35,7 +89,7 @@ function AboutSection({ lang }) {
 
             <div className="topic">
                 <div className='icon-container'>
-                    <AiOutlineUser className='topic-icon' /> ABOUT ME
+                    <AiOutlineUser className='topic-icon' />{texts.aboutText[lang]}
                 </div>
                 <div className='text-container'>
                     {texts.aboutMeText[lang].split('.').map((s,i)=><div key={i}><p>{s+'.'}</p><br/></div>)}
@@ -44,12 +98,24 @@ function AboutSection({ lang }) {
 
             <div className="topic" id="skills-topic">
                 <div className='icon-container' id='skills-icon-container'>
-                    <DiCodeBadge  className='topic-icon' id='skills-icon'/> SKILLS
+                    <DiCodeBadge  className='topic-icon' id='skills-icon'/> {texts.skillsText[lang]}
                 </div>
 
                 <div className='skills-list-container'>
                     <ul className="skills-list">
-                        {texts.skillsText[lang].map((s,i)=><li key={i}>{s}</li>)}
+                        {texts.skillsList[lang].map((s,i)=><li key={i}>{s}</li>)}
+                    </ul>
+                </div>
+           
+            </div>
+            <div className="topic" id="education-topic">
+                <div className='icon-container' id='skills-icon-container'>
+                    <BsMortarboard className='topic-icon' id='skills-icon'/> {texts.educationText[lang]}
+                </div>
+
+                <div className='skills-list-container'>
+                    <ul className="skills-list">
+                        {texts.educationList[lang].map((s,i)=><li key={i}>{s}</li>)}
                     </ul>
                 </div>
            
