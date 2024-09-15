dc053cb9ad0f1c273584c4fc9e8914cb35e8882f
andreprovensi <andreprovensi@gmail.com>
2024-09-15 09:44:48 -0300

    increazing font-size of about text

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -16,6 +16,33 @@ function ProjectsSection({lang}) {
       web_url:"https://diabetes-prediction-mgje.onrender.com/",
       img_url:require("../public/images/diabetes_project.png")
     },
+    {
+      title:{
+        en:"Diabetes Prediction With Machine Learning",
+        pt:"Previsão de Diabetes com Machine Learning"
+      },
+      github_url:"https://github.com/andreprovensi/diabetes_machine_learning_app",
+      web_url:"https://diabetes-prediction-mgje.onrender.com/",
+      img_url:require("../public/images/diabetes_project.png")
+    },
+    {
+      title:{
+        en:"Diabetes Prediction With Machine Learning",
+        pt:"Previsão de Diabetes com Machine Learning"
+      },
+      github_url:"https://github.com/andreprovensi/diabetes_machine_learning_app",
+      web_url:"https://diabetes-prediction-mgje.onrender.com/",
+      img_url:require("../public/images/diabetes_project.png")
+    },
+    {
+      title:{
+        en:"Diabetes Prediction With Machine Learning",
+        pt:"Previsão de Diabetes com Machine Learning"
+      },
+      github_url:"https://github.com/andreprovensi/diabetes_machine_learning_app",
+      web_url:"https://diabetes-prediction-mgje.onrender.com/",
+      img_url:require("../public/images/diabetes_project.png")
+    },
   ]
   
   
